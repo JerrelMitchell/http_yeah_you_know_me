7838bf95cd41bfb8c84508f049b9f276019e0aea
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-04 15:20:44 -0600

    refactor methods and tests to show new implementation of hash for key value pairs of data types and data

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -1,17 +1,31 @@
 class Analyzer
-  attr_reader :verb, :path, :protocol,
-              :host, :port, :origin,
-              :accept
+  attr_reader :data
 
-  def initialize(response_data)
-    @verb     = response_data.split[1]
-    @path     = response_data.split[3]
-    @protocol = response_data.split[5]
-    @host     = response_data.split[7]
-    @port     = response_data.split[9]
-    @origin   = @host
-    # @accept   = response_data.find do |item|
-    #   item.start_with?('Accept:')
-    # end.split[1]
+  def initialize
+    @data = {}
+  end
+
+  def pair_data(lines)
+    split_lines      = lines.split(' ')
+    data['Verb']     = split_lines[1]
+    data['Path']     = split_lines[3]
+    data['Protocol'] = split_lines[5]
+    data['Host']     = split_lines[7]
+    data['Port']     = split_lines[9]
+    data['Origin']   = split_lines[11]
+    data['Accept']   = split_lines[13] << ' ' << split_lines[14]
+  end
+
+  def diagnostic_info
+  %(
+    <pre>
+    Verb: #{data['Verb']}
+    Path: #{data['Path']}
+    Protocol: #{data['Protocol']}
+    Host: #{data['Host']}
+    Port: #{data['Port']}
+    Origin: #{data['Origin']}
+    Accept: #{data['Accept']}
+    </pre>)
   end
 end
--- test/analyzer_test.rb
+++ test/analyzer_test.rb
@@ -6,42 +6,63 @@ require 'pry'
 class AnalyzerTest < Minitest::Test
   attr_reader :analyzer
   def setup
-    @data = %(Verb: GET\n
-        Path: /\n
-        Protocol: HTTP/1.1\n
-        Host: 127.0.0.1\n
-        Port: 9292\n
-        Origin: 127.0.0.1\n
-        Accept: text/html,application/xhtml+xml,application/xml;q=0.9\n
-        image/webp,*/*;q=0.8\n)
-    @analyzer = Analyzer.new(@data)
+    @analyzer = Analyzer.new
+    @lines = %(Verb: GET
+        Path: /
+        Protocol: HTTP/1.1
+        Host: 127.0.0.1
+        Port: 9292
+        Origin: 127.0.0.1
+        Accept: text/html,application/xhtml+xml,application/xml;q=0.9
+        image/webp,*/*;q=0.8)
+    analyzer.pair_data(@lines)
   end
 
   def test_it_exists
     assert_instance_of Analyzer, analyzer
   end
 
-  def test_it_can_get_the_verb
-    assert_equal 'GET', analyzer.verb
+  def test_it_can_get_the_verb_value
+    assert_equal 'GET', analyzer.data['Verb']
   end
 
-  def test_it_can_get_the_path
-    assert_equal '/', analyzer.path
+  def test_it_can_get_the_path_value
+    assert_equal '/', analyzer.data['Path']
   end
 
-  def test_it_can_get_the_protocol
-    assert_equal 'HTTP/1.1', analyzer.protocol
+  def test_it_can_get_the_protocol_value
+    assert_equal 'HTTP/1.1', analyzer.data['Protocol']
   end
 
-  def test_it_can_get_the_host
-    assert_equal '127.0.0.1', analyzer.host
+  def test_it_can_get_the_host_value
+    assert_equal '127.0.0.1', analyzer.data['Host']
   end
 
-  def test_it_can_get_the_port
-    assert_equal '9292', analyzer.port
+  def test_it_can_get_the_port_value
+    assert_equal '9292', analyzer.data['Port']
   end
 
-  def test_it_can_get_the_origin
-    assert_equal '127.0.0.1', analyzer.origin
+  def test_it_can_get_the_origin_value
+    assert_equal '127.0.0.1', analyzer.data['Origin']
+  end
+
+  def test_it_can_get_the_accept_value
+    expected = 'text/html,application/xhtml+xml,'\
+    'application/xml;q=0.9 image/webp,*/*;q=0.8'
+    assert_equal expected, analyzer.data['Accept']
+  end
+
+  def test_it_has_access_to_requested_info
+    expected = %(
+    <pre>
+    Verb: #{analyzer.data['Verb']}
+    Path: #{analyzer.data['Path']}
+    Protocol: #{analyzer.data['Protocol']}
+    Host: #{analyzer.data['Host']}
+    Port: #{analyzer.data['Port']}
+    Origin: #{analyzer.data['Origin']}
+    Accept: #{analyzer.data['Accept']}
+    </pre>)
+    assert_equal expected, analyzer.diagnostic_info
   end
 end
