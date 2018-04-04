95e77984c0db8d53d0e66bfb8f608bf25fca13e8
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-03 23:22:38 -0600

    add tests for module integration and update previous tests for refactoring done on server.rb file

--- test/server_test.rb
+++ test/server_test.rb
@@ -3,68 +3,52 @@ require 'minitest/pride'
 require 'minitest/unit'
 require 'mocha/minitest'
 require './lib/server'
+require 'pry'
 
+# incomplete test suite for server class
 class ServerTest < Minitest::Test
   attr_reader :server
   def setup
-    @server = Server.new
+    @server = Server.new(0000)
   end
 
   def test_it_exists
     assert_instance_of Server, server
   end
 
-  def test_it_has_a_nil_socket_by_default
-    assert_nil server.socket
+  def test_connection_is_off_by_default
+    assert_equal 'off', server.connection
   end
 
-  def test_it_has_empty_lines_by_default
+  def test_it_has_empty_request_lines_by_default
     assert_equal [], server.lines
   end
 
-  def test_it_has_a_nil_request_by_default
-    assert_nil server.request
+  def test_it_can_show_and_add_number_of_requests_to_client
+    expected = "\nHello World! (0)"
+    assert_equal expected, server.hello_response
+
+    10.times do
+      server.add_request
+    end
+
+    expected = "\nHello World! (10)"
+    assert_equal expected, server.hello_response
   end
 
-  def test_it_has_a_nil_client_by_default
-    assert_nil server.client
+  def test_it_has_access_to_headers_array
+    assert_equal ['http/1.1 200 ok'], server.headers
   end
 
-  def test_it_can_show_number_of_visits_to_page
-    expected = "Hello World! (0)\n"
-    assert_equal expected, server.hello_response(0)
-
-    expected = "Hello World! (5)\n"
-    assert_equal expected, server.hello_response(5)
-
-    expected = "Hello World! (10)\n"
-    assert_equal expected, server.hello_response(10)
+  def test_it_has_access_to_requested_info
+    expected = %(Verb: POST\n
+      Path: /\n
+      Protocol: HTTP/1.1\n
+      Host: 127.0.0.1\n
+      Port: 9292\n
+      Origin: 127.0.0.1\n
+      Accept: text/html,application/xhtml+xml,application/xml;q=0.9\n
+      image/webp,*/*;q=0.8\n)
+    assert_equal expected, server.requested_info
   end
-
-  def test_it_can_be_prompted_to_start_with_a_port_number
-    skip
-    server.start(0)
-    assert_instance_of TCPServer, server.client
-  end
-
-  def test_it_can_request_and_add_lines_from_server_socket
-    skip
-  end
-
-  def test_it_has_headers
-    skip
-  end
-
-  def test_it_has_footers
-    skip
-  end
-
-  def test_it_adds_output_to_head_and_body_of_page
-    skip
-  end
-
-  def test_it_adds_preformat_tags_to_output
-    skip
-  end
-
 end
