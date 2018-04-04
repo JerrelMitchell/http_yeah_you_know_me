63b11f4b54af12d61e9045196d98aebc58982832
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-04 15:22:34 -0600

    refactor message methods

--- lib/server_messages.rb
+++ lib/server_messages.rb
@@ -6,15 +6,4 @@ module ServerMessages
   def headers
     ['http/1.1 200 ok']
   end
-
-  def requested_info
-    %(Verb: POST\n
-      Path: /\n
-      Protocol: HTTP/1.1\n
-      Host: 127.0.0.1\n
-      Port: 9292\n
-      Origin: 127.0.0.1\n
-      Accept: text/html,application/xhtml+xml,application/xml;q=0.9\n
-      image/webp,*/*;q=0.8\n)
-  end
 end
