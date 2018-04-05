dbbaa84661d8f15a530a3cab7367aae532d86f74
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-04 22:56:12 -0600

    refactor methods for server tests

--- test/server_test.rb
+++ test/server_test.rb
@@ -3,24 +3,30 @@ require 'minitest/pride'
 require 'minitest/unit'
 require 'mocha/minitest'
 require './lib/server'
-require 'pry'
 
-# incomplete test suite for server class
 class ServerTest < Minitest::Test
   attr_reader :server
   def setup
-    @server = Server.new(0000)
+    @server = Server.new(0)
   end
 
   def test_it_exists
     assert_instance_of Server, server
   end
 
-  def test_connection_is_off_by_default
-    assert_equal 'off', server.connection
+  def test_it_contains_a_router
+    assert_instance_of Router, server.router
+  end
+
+  def test_client_is_nil_by_default
+    assert_nil server.client
   end
 
   def test_it_has_empty_request_lines_by_default
     assert_equal [], server.lines
+  end
+
+  def test_when_started_server_is_not_closed
+    refute server.closed?
   end
 end
