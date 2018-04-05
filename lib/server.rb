551c9dc49783767fca19c3537e1fcca72ff72537
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-04 20:30:13 -0600

    refactor server class and methods to keep functions working

--- lib/server.rb
+++ lib/server.rb
@@ -1,78 +1,29 @@
-require './lib/server_messages'
 require 'socket'
+require './lib/router'
 
 class Server
-  include ServerMessages
-  attr_reader :server, :connection, :lines
+  attr_reader :lines, :server, :client, :router, :closed
   def initialize(port)
-    @server        = TCPServer.new(port)
-    @connection    = 'off'
-    @lines         = []
-    @response      = []
-    @count         = 0
-  end
-
-  def connect
-    @connection = server.accept
-  end
-
-  def request_lines(connection)
-    line = connection.gets.chomp
-    until line.empty?
-      line = connection.gets.chomp
-      @lines << line
-    end
-  end
-
-  def analyze(lines)
-    @analyzed_lines = Analyzer.new(lines)
-  end
-
-  def close
-    @connection.close
-  end
-
-  def add_request
-    @count += 1
+    @server = TCPServer.new(port)
+    @router = Router.new(self)
+    @client = nil
+    @closed = false
   end
 
   def start
     loop do
-      connect
-      request_lines(connection)
-      add_request
-      # analyze(lines)
-      connection.puts headers
-      connection.puts hello_response
-      # connection.puts diagnostic_info
-      # route_response
-      close
+      break if closed == true
+      @client = server.accept
+      @lines  = []
+      request_lines
+      router.verb_router
+      client.close
     end
   end
 
-  def path_conditional(path)
-  end
-
-  def route_response
-    if path == '/'
-      requested_info
-    elsif path == '/hello'
-      hello_response
-    elsif path == '/datetime'
-      Time.now.strftime('%I:%M%p on %A, %B %d, %Y')
-    elsif path == '/shutdown'
-      "Total Requests: #{@count}" && exit
-    else
-      "404 Page Not Found"
+  def request_lines
+    while line = @client.gets and !line.chomp.empty?
+      @lines << line.chomp
     end
   end
-
-  # If they request the root, aka /, respond with just the debug info from Iteration 1.
-  # If they request /hello, respond with “Hello, World! (0)” where the 0
-  # increments each time the path is requested, but not when any other path is requested.
-  # If they request /datetime, respond with today’s date and time in this format: 11:07AM on Sunday, November 1, 2015.
-  # If they request /shutdown, respond with “Total Requests: 12” where 12 is
-  # the aggregate of all requests. It also causes the server to exit / stop serving requests.
-
-
 end
